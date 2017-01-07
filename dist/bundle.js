ff7d10027e47885924ecaa6e84dd68f8e97efe86
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2017-01-07 17:07:45 -0500

    adds test helper file

--- dist/bundle.js
+++ dist/bundle.js
@@ -44,14 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
-
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	console.log("Help, I'm alive!");
+	(function webpackMissingModule() { throw new Error("Cannot find module \"./src/index.js\""); }());
 
 
 /***/ }
